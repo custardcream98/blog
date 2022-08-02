d65f591b2fe787effd0c150d51c06be50f1ebb0b
custardcream@kakao.com <custardcream@kakao.com>
2022-08-02 13:24:05 +0900

    Feature: Add remark-attr renderer

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,9 +1,10 @@
 import { remark } from 'remark'
 import remarkRehype from "remark-rehype";
+import remarkAttr from "remark-attr"
 import rehypeStringify from "rehype-stringify";
 import rehypePrettyCode from "rehype-pretty-code";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(remarkRehype).use(rehypePrettyCode).use(rehypeStringify).process(markdown)
+  const result = await remark().use(remarkAttr.remarkAttr).use(remarkRehype).use(rehypePrettyCode).use(rehypeStringify).process(markdown)
   return result.toString()
 }
