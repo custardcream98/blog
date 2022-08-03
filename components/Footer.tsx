fc97d70e44f3a3f86e3e266980eb0f8f408ba59b
custardcream@kakao.com <custardcream@kakao.com>
2022-08-03 10:13:50 +0900

    Chore: Change Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,26 @@
 import styled from "styled-components";
+import { LinkDecorated } from "./styledComponents";
 
 const Container = styled.div`
   height: 5rem;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   box-shadow: ${(props) => props.theme.footerShadow};
-  a {
-    color: ${(props) => props.theme.textColor};
-    font-size: 0.7rem;
-    font-weight: 300;
-  }
-  a:hover {
-    text-decoration: underline;
+  font-size: 0.7rem;
+  font-weight: 300;
+  div {
+    margin-top: 0.5rem;
+    a {
+      color: ${(props) => props.theme.textColor};
+      font-size: inherit;
+      font-weight: inherit;
+    }
+    a:not(:nth-child(1)) {
+      padding-left: 0.3rem;
+    }
   }
 `;
 
@@ -21,7 +28,21 @@ const Footer = () => {
   return (
     <footer>
       <Container>
-        <a href="https://github.com/custardcream98">@custardcream98 Github</a>
+        &copy; {new Date().getFullYear()} custardcream98. All rights reserved.
+        <div>
+          <LinkDecorated
+            href="https://github.com/custardcream98"
+            target="_blank"
+          >
+            Github
+          </LinkDecorated>
+          <LinkDecorated
+            href="https://www.linkedin.com/in/shi-woo-park-668b33147/"
+            target="_blank"
+          >
+            LinkedIn
+          </LinkDecorated>
+        </div>
       </Container>
     </footer>
   );
