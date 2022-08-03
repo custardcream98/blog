b2b4c8fcb00c5773d3e3ec67643e4e99edc77c47
custardcream@kakao.com <custardcream@kakao.com>
2022-08-03 14:12:12 +0900

    Chore: Add Firebase category theme

--- lib/categoryTheme.ts
+++ lib/categoryTheme.ts
@@ -11,7 +11,8 @@ const categoryTheme:ICategoryTheme = {
   Python: { backgroundColor: "#ffc9074e", borderColor: "#ffc907" },
   "Coding Test": { backgroundColor: "#ff076e4e", borderColor: "#ff076e" },
   "Next.js": { backgroundColor: "#d1d1d14e", borderColor: "#d1d1d1" },
-  JavaScript: {backgroundColor: "#ffea074e", borderColor: "#ffea07"}
+  JavaScript: { backgroundColor: "#ffea074e", borderColor: "#ffea07" },
+  Firebase: {backgroundColor:"#dea41f4e", borderColor:"#dea41f"}
 }
 
 export default categoryTheme;
