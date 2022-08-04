75f5c46e98ea456f3d53e5fe34cae0d51f2527bb
custardcream@kakao.com <custardcream@kakao.com>
2022-08-05 02:44:53 +0900

    Chore: Delete unnecessary import

--- lib/firebaseSetup/index.ts
+++ lib/firebaseSetup/index.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -13,5 +12,4 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig);
 
-export const fireAuth = getAuth();
 export const fireStore = getFirestore();
