4b8a6ff21d43197c2db35798633c7807706a2e7c
custardcream@kakao.com <custardcream@kakao.com>
2022-08-06 16:30:55 +0900

    Bugfix: Generate Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://custardcream.vercel.app/",
   generateRobotsTxt: true,
   priority: 1.0,
-  outDir: "./out",
   generateIndexSitemap: false,
 };
