e10a2b87c5449c46a7d365b217a2426dc86d8148
custardcream@kakao.com <custardcream@kakao.com>
2022-08-15 22:43:21 +0900

    Feature: Add rehype-raw

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,11 +1,12 @@
 import { remark } from 'remark'
 import remarkRehype from "remark-rehype";
-import rehypeSlug from "rehype-slug"
-import rehypeToc from '@jsdevtools/rehype-toc'
+import rehypeRaw from "rehype-raw";
+import rehypeSlug from "rehype-slug";
+import rehypeToc from '@jsdevtools/rehype-toc';
 import rehypeStringify from "rehype-stringify";
 import rehypePrettyCode from "rehype-pretty-code";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(remarkRehype).use(rehypeSlug).use(rehypeToc).use(rehypePrettyCode).use(rehypeStringify).process(markdown)
+  const result = await remark().use(remarkRehype, {allowDangerousHtml:true}).use(rehypeRaw).use(rehypeSlug).use(rehypeToc).use(rehypePrettyCode).use(rehypeStringify).process(markdown)
   return result.toString()
 }
