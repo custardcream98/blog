36d3e2aca18a16cbf2522180eb55165f109ed2cd
custardcream@kakao.com <custardcream@kakao.com>
2022-08-16 01:05:49 +0900

    Feature: Add anchor target blank plugin

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -5,8 +5,21 @@ import rehypeSlug from "rehype-slug";
 import rehypeToc from '@jsdevtools/rehype-toc';
 import rehypeStringify from "rehype-stringify";
 import rehypePrettyCode from "rehype-pretty-code";
+import { visit } from 'unist-util-visit';
+import { Root } from 'hast'
+import { Transformer } from "unified";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(remarkRehype, {allowDangerousHtml:true}).use(rehypeRaw).use(rehypeSlug).use(rehypeToc).use(rehypePrettyCode).use(rehypeStringify).process(markdown)
+  const result = await remark().use(remarkRehype, {allowDangerousHtml:true}).use(anchorTargetBlank).use(rehypeRaw).use(rehypeSlug).use(rehypeToc).use(rehypePrettyCode).use(rehypeStringify).process(markdown)
   return result.toString()
+}
+
+function anchorTargetBlank():Transformer<Root, Root> {
+  return (tree) => {
+    visit(tree, 'element', (node) => {
+      if (node.tagName === 'a') {
+        node.properties = { ...node.properties, target: "_blank" };
+      }
+    })
+  }
 }
