a2f589eb29d9178fd9d535fc0f89eea30f71421c
custardcream@kakao.com <custardcream@kakao.com>
2022-08-28 13:57:29 +0900

    Feature: Change font provider to Fontsource

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import type { AppProps } from "next/app";
 
+import "@fontsource/noto-sans";
+import "@fontsource/noto-sans-kr";
+import "@fontsource/nanum-myeongjo";
+import "@fontsource/source-code-pro/800.css";
+import "@fontsource/poppins/800.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
