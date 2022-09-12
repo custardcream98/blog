09c6ba3ee2ac645aa13d2891219ed0a99f7797b3
shiwoo.park <custardcream@kakao.com>
2022-09-13 01:34:23 +0900

    Bugfix: max-width 추가

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -18,7 +18,7 @@ import { createPostDoc } from "../../lib/firebaseSetup/firebaseApps";
 import styled from "styled-components";
 
 const PostSection = styled.section`
-  width: inherit;
+  max-width: 800px;
 `;
 
 type Props = {
