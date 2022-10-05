ba4c538d23d865f991fdec7600e7697b97399db8
shiwoo.park <87423085+custardcream98@users.noreply.github.com>
2022-10-05 14:01:01 +0900

    Chore: 콘솔창에 메세지 남기기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,28 @@ import "../styles/reset.css";
 import "../styles/font.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.clear();
+
+  console.log(
+    "%c안녕하세요👋",
+    "font-family: 'Noto Sans'; font-size: 16px; font-weight: 300"
+  );
+
+  console.log(
+    "%c주니어 프론트엔드 개발자",
+    "display: inline-block; font-family: 'Noto Sans'; font-size: 24px; font-weight: 800; color: #0938F0;"
+  );
+
+  console.log(
+    "%c박시우입니다.",
+    "display: inline-block; font-family: 'Noto Sans'; font-size: 24px; font-weight: 800; background-image: linear-gradient(to right, #0938F0, #03B8F4); color: #ffffff; padding: 6px 8px 2px 11px;"
+  );
+
+  console.log(
+    "%c이력서 : https://custardcream98.github.io/resume/\n이메일 : custardcream@kakao.com",
+    "font-family: 'Noto Sans'; font-size: 15px; font-weight: 500"
+  );
+
   return (
     <RecoilRoot>
       <Component {...pageProps} />
