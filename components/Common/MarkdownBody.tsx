eec3465c30120b8ced593266459918afa9d75993
shiwoo.park <87423085+custardcream98@users.noreply.github.com>
2022-10-05 17:11:58 +0900

    Chore: Change Post's anchor hover style

--- components/Common/MarkdownBody.tsx
+++ components/Common/MarkdownBody.tsx
@@ -223,6 +223,36 @@ const MarkdownBody = styled.div`
     margin: 2rem 0;
   }
 
+  a {
+    position: relative;
+    color: ${(props) => props.theme.accentColor};
+    transition: linear 0.3s;
+    -webkit-transition: linear 0.3s;
+    -moz-transition: linear 0.3s;
+    z-index: 10;
+    &:hover {
+      text-decoration: none;
+      color: ${(props) => props.theme.bgColor};
+    }
+    &::before {
+      content: " ";
+      position: absolute;
+      width: calc(100% + 0.6rem);
+      height: calc(100% + 0.2rem);
+      top: -0.1rem;
+      left: -0.3rem;
+      border-radius: 4px;
+      background-color: transparent;
+      transition: linear 0.3s;
+      -webkit-transition: linear 0.3s;
+      -moz-transition: linear 0.3s;
+      z-index: -1;
+    }
+    &:hover::before {
+      background-color: ${(props) => props.theme.textColor};
+    }
+  }
+
   .toc {
     width: fit-content;
     background-color: #1e1e1e;
@@ -232,14 +262,12 @@ const MarkdownBody = styled.div`
     font-size: calc(var(--main-font-size) * 0.9);
     border-radius: 4px;
     a {
-      text-decoration: none;
-      color: white;
-      transition: linear 0.3s;
-      -webkit-transition: linear 0.3s;
-      -moz-transition: linear 0.3s;
+      color: inherit;
       &:hover {
         color: #1e1e1e;
-        background-color: #dedede;
+      }
+      &:hover::before {
+        background-color: white;
       }
     }
     ol > li::before {
--- lib/theme.ts
+++ lib/theme.ts
@@ -2,29 +2,30 @@ import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
   bgColor: "#efefef",
-  textColor: "#1a1a1a",
-  subTextColor:"rgb(177, 177, 177)",
+  textColor: "#121212",
+  subTextColor: "rgb(177, 177, 177)",
   navLineShadow: "rgba(17, 17, 26, 0.1) 0px 1px 0px",
-  footerShadow:"rgba(17, 17, 26, 0.1) 1px 0px 0px",
+  footerShadow: "rgba(17, 17, 26, 0.1) 1px 0px 0px",
   accentColor: "#0070f3",
-  mainColor:"#d7a306",
+  mainColor: "#d7a306",
   mainGradient: "linear-gradient(90deg, #d7a306, #fbf2c8)",
   codingFont: '"Source Code Pro", monospace',
-  mainFont: '"Noto Sans", "Noto Sans KR", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
-  titleFont: '"Poppins", sans-serif'
-}
+  mainFont:
+    '"Noto Sans", "Noto Sans KR", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+  titleFont: '"Poppins", sans-serif',
+};
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "black",
+  bgColor: "#121212",
   textColor: "#efefef",
-  subTextColor:"rgb(177, 177, 177)",
+  subTextColor: "rgb(177, 177, 177)",
   navLineShadow: "rgba(200, 200, 200, 0.25) 0px 1px 0px",
-  footerShadow:"rgba(17, 17, 26, 0.1) 1px 0px 0px",
+  footerShadow: "rgba(17, 17, 26, 0.1) 1px 0px 0px",
   accentColor: "#0070f3",
-  mainColor:"#d7a306",
+  mainColor: "#d7a306",
   mainGradient: "linear-gradient(90deg, #d7a306, #fbf2c8)",
   codingFont: '"Source Code Pro", monospace',
-  mainFont: '"Noto Sans", "Noto Sans KR", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
-  titleFont: '"Poppins", sans-serif'
-}
-
+  mainFont:
+    '"Noto Sans", "Noto Sans KR", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+  titleFont: '"Poppins", sans-serif',
+};
