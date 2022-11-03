bfa36e114057ed03ea1e48621d01866d13e87a94
shiwoo.park <87423085+custardcream98@users.noreply.github.com>
2022-11-03 14:40:00 +0900

    Feature: useWindowSize 커스텀 훅

---
+++ lib/hook/useWindowSize.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+/**
+ * resize 이벤트 발생을 listen해 윈도우 사이즈 변경을 확인할 수 있도록 하는 커스텀 훅
+ * @returns windowSize { width: number, height: number }
+ */
+export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState<{ [key: string]: number | undefined }>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = () =>
+      setWindowSize((_) => ({ width: window.innerWidth, height: window.innerHeight }));
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize(); // useEffect 첫 동작시 바로 확인
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+};
