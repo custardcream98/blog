e29f52287894ab7fff55d01dcc081ed01c0a2a24
shiwoo.park <custardcream@kakao.com>
2023-01-20 16:42:57 +0900

    Style: 테마 중복 제거 및 리셋 스타일 변경

--- lib/theme.ts
+++ lib/theme.ts
@@ -2,7 +2,20 @@ import { DefaultTheme } from "styled-components";
 
 // TODO: 테마 관리 방법 리팩토링
 
+const commonTheme = {
+  footerShadow: "rgba(17, 17, 26, 0.1) 1px 0px 0px",
+
+  mainColor: "#d7a306",
+  mainGradient: "linear-gradient(90deg, #d7a306, #fbf2c8)",
+  codingFont: '"Source Code Pro", monospace',
+  mainFont:
+    '"Noto Sans", "Noto Sans KR", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+  titleFont: '"Poppins", sans-serif',
+};
+
 export const lightTheme: DefaultTheme = {
+  ...commonTheme,
+
   bgColor: "#fcfcfc",
   textColor: "#121212",
   subTextColor: "rgb(140, 140, 140)",
@@ -13,17 +26,12 @@ export const lightTheme: DefaultTheme = {
 
   darkmodeShadow:
     "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
-  footerShadow: "rgba(17, 17, 26, 0.1) 1px 0px 0px",
   accentColor: "#0070f3",
-  mainColor: "#d7a306",
-  mainGradient: "linear-gradient(90deg, #d7a306, #fbf2c8)",
-  codingFont: '"Source Code Pro", monospace',
-  mainFont:
-    '"Noto Sans", "Noto Sans KR", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
-  titleFont: '"Poppins", sans-serif',
 };
 
 export const darkTheme: DefaultTheme = {
+  ...commonTheme,
+
   bgColor: "#121212",
   textColor: "#efefef",
   subTextColor: "rgb(177, 177, 177)",
@@ -34,12 +42,5 @@ export const darkTheme: DefaultTheme = {
 
   darkmodeShadow:
     "rgba(220, 205, 205, 0.25) 0px 2px 5px -1px, rgba(255, 255, 255, 0.3) 0px 1px 3px -1px",
-  footerShadow: "rgba(17, 17, 26, 0.1) 1px 0px 0px",
   accentColor: "#3b96ff",
-  mainColor: "#d7a306",
-  mainGradient: "linear-gradient(90deg, #d7a306, #fbf2c8)",
-  codingFont: '"Source Code Pro", monospace',
-  mainFont:
-    '"Noto Sans", "Noto Sans KR", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
-  titleFont: '"Poppins", sans-serif',
 };
