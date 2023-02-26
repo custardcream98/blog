93ae6bb4115478e0ac2076c19a8698cb6f8e839c
shiwoo.park <custardcream@kakao.com>
2023-02-27 04:24:57 +0900

    Refactor: LinkIcon 컴포넌트로 분리

---
+++ components/Common/LinkIcon.tsx
@@ -0,0 +1,53 @@
+import styled, { css } from "styled-components";
+
+import type { ComponentPropsWithoutRef } from "react";
+
+type Props = {
+  title: string;
+  id: string;
+  size?: string;
+} & ComponentPropsWithoutRef<"svg">;
+
+type StyledSvgProps = {
+  size: string;
+};
+
+const LinkIcon = ({
+  title,
+  id,
+  size = "100%",
+  ...props
+}: Props) => {
+  return (
+    <StyledSvg
+      className="link-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="none"
+      aria-labelledby={id}
+      size={size}
+      {...props}
+    >
+      <title id={id}>{title}</title>
+      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+      <polyline points="15 3 21 3 21 9"></polyline>
+      <line x1="10" y1="14" x2="21" y2="3"></line>
+    </StyledSvg>
+  );
+};
+
+const StyledSvg = styled.svg<StyledSvgProps>`
+  ${({ size }) => css`
+    width: ${size};
+    height: ${size};
+  `}
+  stroke: ${({ theme }) => theme.textColor};
+  :hover {
+    stroke: ${({ theme }) => theme.accentColor};
+  }
+`;
+
+export default LinkIcon;
--- components/Searchbar/LinkToPost.tsx
+++ components/Searchbar/LinkToPost.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import styled from "styled-components";
 
 import { cssOutlineOnFocus } from "components/Layout/Navigation/styles";
+import LinkIcon from "components/Common/LinkIcon";
 
 const StyledLink = styled.a`
   display: block;
@@ -46,21 +47,10 @@ export default function LinkToPost({
         className="result-link"
         onClick={closeResults}
       >
-        <svg
-          className="link-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          fill="none"
-          aria-labelledby={id}
-        >
-          <title id={id}>{title} 포스트로 이동하기</title>
-          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-          <polyline points="15 3 21 3 21 9"></polyline>
-          <line x1="10" y1="14" x2="21" y2="3"></line>
-        </svg>
+        <LinkIcon
+          id={id}
+          title={`${title} 포스트로 이동하기`}
+        />
       </StyledLink>
     </Link>
   );
