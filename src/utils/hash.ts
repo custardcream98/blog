0ce91a4d379a06dca630f5f2efaf3bd5b04f5ac8
shiwoo.park <custardcream@kakao.com>
2023-06-12 04:37:16 +0900

    feat: hash 알고리즘 변경

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 /**
  * https://gist.github.com/jlevy/c246006675becc446360a798e2b2d781
  *
@@ -5,11 +7,8 @@
  * @returns 해시값
  */
 export const hash = (target: string): string => {
-  let hash = 0;
-  for (let i = 0; i < target.length; i++) {
-    const char = target.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash &= hash;
-  }
-  return new Uint32Array([hash])[0].toString(36);
+  return crypto
+    .createHash("shake256", { encoding: "utf-8", outputLength: 5 })
+    .update(target)
+    .digest("hex");
 };
