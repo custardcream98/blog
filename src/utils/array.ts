d3499ae0c976728c812adb41fdf79279d958663d
shiwoo.park <custardcream@kakao.com>
2023-06-21 00:12:40 +0900

    feat: calculateLoopedIndex 유틸

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,10 @@
 export const generateNumberArray = (length: number, startsWith = 0) => {
   return new Array(length).fill(0).map((_, index) => index + startsWith);
 };
+
+/**
+ * 루프 구조의 배열에서 currentIndex에서 offset만큼 떨어진 index를 반환합니다.
+ */
+export const calculateLoopedIndex = (currentIndex: number, offset: number, length: number) => {
+  return (currentIndex + offset + length) % length;
+};
