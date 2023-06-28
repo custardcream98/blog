478f233950693f74032020a0345ddbc4600f2485
shiwoo.park <custardcream@kakao.com>
2023-06-28 13:46:14 +0900

    chore: lint fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_HOST,
+  generateIndexSitemap: false,
   generateRobotsTxt: true,
   priority: 1.0,
-  generateIndexSitemap: false,
+  siteUrl: process.env.NEXT_PUBLIC_HOST,
 };
