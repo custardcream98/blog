5740607d4205be17ac59b1cc21cd92036a39805c
shiwoo.park <custardcream@kakao.com>
2023-06-28 13:19:44 +0900

    feat: getCurrentURL 유틸

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export * from "./string";
 export * from "./date";
 export * from "./event";
 export * from "./copy";
+export * from "./url";
---
+++ src/utils/url.ts
@@ -0,0 +1,3 @@
+export const getCurrentURL = () => {
+  return window.location.href;
+};
