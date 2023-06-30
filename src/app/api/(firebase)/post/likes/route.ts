43400f5188596302d090a93bb84cadae8fb177d2
shiwoo.park <custardcream@kakao.com>
2023-06-30 13:55:57 +0900

    feat: patch like api

--- src/app/api/(firebase)/post/likes/route.ts
+++ src/app/api/(firebase)/post/likes/route.ts
@@ -1,8 +1,9 @@
-import { encodeToPercentString, parseSearchParams } from "src/utils";
+import { encodeToPercentString, getRequestBody, parseSearchParams } from "src/utils";
 
 import { TitleRequest } from "../../_types";
-import { getDoc, getDocData, getPostDocRef } from "../../_utils";
+import { getDoc, getDocData, getPostDocRef, updateDoc } from "../../_utils";
 
+import { FieldValue } from "firebase-admin/firestore";
 import { StatusCodes } from "http-status-codes";
 import { NextResponse } from "next/server";
 
@@ -22,4 +23,25 @@ export async function GET(request: Request): Promise<NextResponse> {
   const { likes } = getDocData(postDoc);
 
   return NextResponse.json({ data: { likes } });
+}
+
+export async function PATCH(request: Request): Promise<NextResponse> {
+  const { title } = await getRequestBody<TitleRequest>(request);
+
+  if (!title) {
+    return NextResponse.json(
+      { message: "잘못된 요청입니다. (title이 없습니다.)" },
+      { status: StatusCodes.BAD_REQUEST },
+    );
+  }
+
+  const encodedTitle = encodeToPercentString(title);
+  const postDocRef = getPostDocRef(encodedTitle);
+
+  await updateDoc(postDocRef, { likes: FieldValue.increment(1) });
+
+  const postDoc = await getDoc(postDocRef);
+  const { likes } = getDocData(postDoc);
+
+  return NextResponse.json({ data: { likes }, message: "Like 성공" });
 }
