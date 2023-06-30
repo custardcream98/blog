f3582999787f21b25878818f6184a0fd08a99618
shiwoo.park <custardcream@kakao.com>
2023-06-30 20:00:52 +0900

    refactor: useLocalStorageState에서 불필요한 ref 제거

--- src/hook/useLocalStorageState.ts
+++ src/hook/useLocalStorageState.ts
@@ -1,16 +1,23 @@
-import { type Dispatch, type SetStateAction, useCallback, useRef, useState } from "react";
+import { type Dispatch, type SetStateAction, useCallback, useState } from "react";
+
+const getLocalStorageValue = <T>(key: string) => {
+  try {
+    const value = localStorage.getItem(key);
+    const parsedValue = value !== null ? JSON.parse(value) : null;
+    return parsedValue as T | null;
+  } catch (error) {
+    return null;
+  }
+};
 
 export const useLocalStorageState = <T>(
   key: string,
   initialValue: T,
 ): [storedValue: T, setValue: Dispatch<SetStateAction<T>>] => {
-  const storedValueRef = useRef<T>(initialValue);
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
-      const value = localStorage.getItem(key);
-      const parsedValue = value !== null ? JSON.parse(value) : initialValue;
-      storedValueRef.current = parsedValue;
-      return parsedValue;
+      const localStorageValue = getLocalStorageValue<T>(key);
+      return localStorageValue ?? initialValue;
     } catch (error) {
       return initialValue;
     }
@@ -18,12 +25,12 @@ export const useLocalStorageState = <T>(
 
   const setValue: Dispatch<SetStateAction<T>> = useCallback(
     (value) => {
-      const valueToStore = value instanceof Function ? value(storedValueRef.current) : value;
+      setStoredValue((prevValue) => {
+        const nextValue = value instanceof Function ? value(prevValue) : value;
+        localStorage.setItem(key, JSON.stringify(nextValue));
 
-      localStorage.setItem(key, JSON.stringify(valueToStore));
-
-      setStoredValue(valueToStore);
-      storedValueRef.current = valueToStore;
+        return nextValue;
+      });
     },
     [key],
   );
