da23e0d4f88041e92273b27d0b7a3b75de13e23c
shiwoo.park <custardcream@kakao.com>
2023-06-30 14:24:35 +0900

    feat: patch like가 unlike도 대응하도록 개선

--- src/app/api/(firebase)/post/likes/route.ts
+++ src/app/api/(firebase)/post/likes/route.ts
@@ -25,20 +25,24 @@ export async function GET(request: Request): Promise<NextResponse> {
   return NextResponse.json({ data: { likes } });
 }
 
+type PatchLikesRequest = {
+  shouldLike: boolean;
+} & TitleRequest;
 export async function PATCH(request: Request): Promise<NextResponse> {
-  const { title } = await getRequestBody<TitleRequest>(request);
+  const { title, shouldLike } = await getRequestBody<PatchLikesRequest>(request);
 
-  if (!title) {
+  if (!title || shouldLike === undefined) {
     return NextResponse.json(
-      { message: "잘못된 요청입니다. (title이 없습니다.)" },
+      { message: "잘못된 요청입니다. (title 혹은 shouldLike가 없습니다.)" },
       { status: StatusCodes.BAD_REQUEST },
     );
   }
 
   const encodedTitle = encodeToPercentString(title);
   const postDocRef = getPostDocRef(encodedTitle);
+  const increment = shouldLike ? 1 : -1;
 
-  await updateDoc(postDocRef, { likes: FieldValue.increment(1) });
+  await updateDoc(postDocRef, { likes: FieldValue.increment(increment) });
 
   const postDoc = await getDoc(postDocRef);
   const { likes } = getDocData(postDoc);
