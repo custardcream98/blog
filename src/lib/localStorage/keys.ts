0f97eae48dcdf44480c684f3a0f67acbea2f0317
shiwoo.park <custardcream@kakao.com>
2023-06-30 20:02:28 +0900

    refactor: localStorage 관련 코드 제거

--- src/lib/localStorage/keys.ts
+++
@@ -1,8 +0,0 @@
-export const KEY_IS_LIKED = "isLiked";
-export const KEY_LAST_VIEWED = "lastViewed";
-
-export enum LocalStorageKeys {
-  IS_LIKED = "isLiked",
-  LAST_VIEWED = "views",
-  IS_DARKMODE_ACTIVATED = "isDarkmodeActivated",
-}
