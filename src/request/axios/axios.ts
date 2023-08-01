2e0aa93f25e09f98865912bcb15a2187539f3260
shiwoo.park <custardcream@kakao.com>
2023-08-02 02:01:27 +0900

    chore: axios instance에서 상대 경로 사용

--- src/request/axios/axios.ts
+++ src/request/axios/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const nextApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_HOST + "/api",
+  baseURL: "/api",
   headers: { "Content-Type": "application/json" },
 });
