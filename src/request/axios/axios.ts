59ec793c861893c5d0f26845103ca6685fce02fc
shiwoo.park <custardcream@kakao.com>
2023-08-02 02:01:27 +0900

    Revert "chore: axios instance에서 상대 경로 사용"
    This reverts commit 2e0aa93f25e09f98865912bcb15a2187539f3260.

--- src/request/axios/axios.ts
+++ src/request/axios/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const nextApi = axios.create({
-  baseURL: "/api",
+  baseURL: process.env.NEXT_PUBLIC_HOST + "/api",
   headers: { "Content-Type": "application/json" },
 });
