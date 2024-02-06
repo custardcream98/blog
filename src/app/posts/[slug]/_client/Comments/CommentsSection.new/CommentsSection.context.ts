e783d7738654bbed8be8c0abd256ccdc0a378ecb
shiwoo.park <custardcream@kakao.com>
2024-02-07 06:49:34 +0900

    refactor(comments): CommentsSection 컴포넌트 파일 분리

---
+++ src/app/posts/[slug]/_client/Comments/CommentsSection.new/CommentsSection.context.ts
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+
+export const CommentPostTitleContext = React.createContext("");
+
+export const useCommentPostTitleContext = () => {
+  const postTitle = useContext(CommentPostTitleContext);
+  return postTitle;
+};
---
+++ src/app/posts/[slug]/_client/Comments/CommentsSection.new/CommentsSection.tsx
@@ -0,0 +1,21 @@
+import { CommentPostTitleContext } from "./CommentsSection.context";
+
+import { utld } from "utility-class-components";
+
+type Props = React.PropsWithChildren<{
+  postTitle: string;
+}>;
+
+export function CommentsSection({ children, postTitle }: Props) {
+  return (
+    <CommentPostTitleContext.Provider value={postTitle}>
+      <Wrapper>{children}</Wrapper>
+    </CommentPostTitleContext.Provider>
+  );
+}
+
+const Wrapper = utld.section`
+  flex
+  flex-col
+  w-full
+`;
