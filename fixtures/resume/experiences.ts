9bd347eddb9ce76d5c56d948bbafdb437853ad3a
shiwoo.park <custardcream@kakao.com>
2024-04-10 02:11:43 +0900

    docs(resume): 일부 내용 삭제

--- fixtures/resume/experiences.ts
+++ fixtures/resume/experiences.ts
@@ -19,35 +19,35 @@ const experiences: ResumeSectionData[] = [
     title: "『타입스크립트 넌 내 타입』 집필",
   },
   {
-    descriptions: [
-      "정기적으로 Frontend Tech Talk을 진행했습니다.",
-      "신입 멤버 지원서를 효율적으로 관리할 수 있는 웹 페이지를 Next.js 활용해 개발했습니다.",
-      "Frontend 팀의 Core Member로서 Daily Scrum, Sprint 회고, Kick Off, CPU Meeting 등을 주도했습니다.",
-    ],
     period: {
       from: "2022-03",
       to: "2023-02",
     },
+    points: [
+      // "정기적으로 Frontend Tech Talk을 진행했습니다.",
+      // "신입 멤버 지원서를 효율적으로 관리할 수 있는 웹 페이지를 Next.js 활용해 개발했습니다.",
+      "Frontend 팀의 Core Member로서 Daily Scrum, Sprint 회고, Kick Off, CPU Meeting 등을 주도했습니다.",
+    ],
     title: "Google Developer Student Clubs UOS",
   },
   {
-    descriptions: [
-      "인천시 생활폐기물 발생량 분석 및 예측 모델 개발 연구, 착한 이륜차 운전자 평가 시스템 개발 연구에 참여했습니다.",
-      "Web Developer로서 연구에 필요한 설문 폼을 개발했습니다.",
-    ],
     period: {
       from: "2021-06",
       to: "2023-02",
     },
+    points: [
+      "인천시 생활폐기물 발생량 분석 및 예측 모델 개발 연구, 착한 이륜차 운전자 평가 시스템 개발 연구에 참여했습니다.",
+      // "Web Developer로서 연구에 필요한 설문 폼을 개발했습니다.",
+    ],
     title: "서울시립대학교 공간 데이터베이스 연구실 학부생 연구원",
   },
-  {
-    period: { from: "2023-01", to: "2023-01" },
-    points: [
-      "멋쟁이사자처럼 FE School의 우수 수료생으로 선정되어 특별상, 콘텐츠상, 동료칭찬상을 수상했습니다.",
-    ],
-    title: "부트캠프 우수 수료생",
-  },
+  // {
+  //   period: { from: "2023-01", to: "2023-01" },
+  //   points: [
+  //     "멋쟁이사자처럼 FE School의 우수 수료생으로 선정되어 특별상, 콘텐츠상, 동료칭찬상을 수상했습니다.",
+  //   ],
+  //   title: "부트캠프 우수 수료생",
+  // },
   // {
   //   links: [
   //     {
