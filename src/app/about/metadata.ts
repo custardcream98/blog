5821174aa7c713761cab08740be5e49c3bc6625f
shiwoo.park <custardcream@kakao.com>
2025-07-27 05:28:21 +0900

    chore: metadata 수정

--- src/app/about/metadata.ts
+++ src/app/about/metadata.ts
@@ -2,9 +2,8 @@ import { type Metadata } from "next"
 
 import { sharedMetadata } from "../sharedMetadata"
 
-const META_TITLE = "소개 - 프론트엔드 개발자 박시우"
-const META_DESCRIPTION =
-  "안녕하세요! 동료와 함께 성장하며 사용자 경험을 개선하는 프론트엔드 개발자 박시우입니다."
+const META_TITLE = "프론트엔드 개발자 박시우"
+const META_DESCRIPTION = "Done is better than perfect 를 모토로 삼고 있습니다."
 
 export const metadata: Metadata = {
   ...sharedMetadata,
