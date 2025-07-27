9fce88aabf10b938f20b10b393b93cacf66094ef
shiwoo.park <custardcream@kakao.com>
2025-07-28 03:46:39 +0900

    feat: 블로그 글 목록도 revalidate

--- src/app/api/revalidate-post/route.ts
+++ src/app/api/revalidate-post/route.ts
@@ -19,6 +19,7 @@ export async function POST(request: NextRequest) {
   }
 
   revalidatePath(`/posts/${slug}`)
+  revalidatePath("/", "page")
 
   return NextResponse.json({
     message: "Revalidated successfully",
