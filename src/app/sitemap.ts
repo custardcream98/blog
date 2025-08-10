1891f3ecd315074c8c4e25b70b7917ddeb7897ea
shiwoo.park <custardcream@kakao.com>
2025-08-10 13:24:14 +0900

    feat: sitemap 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,21 +1,25 @@
+import { normalizeScrapDataByMonth } from "@/domains/scrap/utils/normalize"
 import { getPostsList } from "@/lib/octokit/blog"
+import { getScrapsList } from "@/lib/octokit/scraps"
+import { objectKeys } from "@/utils/types"
 
 export const dynamic = "force-dynamic"
 
 export default async function sitemap() {
   const posts = await getPostsList()
+  const scrapDataMonths = objectKeys(normalizeScrapDataByMonth(await getScrapsList()))
 
   return [
     ...posts.map((post) => ({
       url: `https://shiwoo.dev/posts/${post.slug}`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 1,
     })),
     {
       url: "https://shiwoo.dev",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
@@ -30,5 +34,21 @@ export default async function sitemap() {
       changeFrequency: "monthly",
       priority: 1,
     },
+    {
+      url: "https://shiwoo.dev/scraps",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    ...scrapDataMonths.map((yyyymm) => {
+      const [year, month] = yyyymm.split("-")
+
+      return {
+        url: `https://shiwoo.dev/scraps/${year}/${month}`,
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 1,
+      }
+    }),
   ]
 }
