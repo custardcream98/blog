e1cee27eac3c6f5399aaccdf2f89a3251a1712b5
shiwoo.park <custardcream@kakao.com>
2025-09-06 02:20:42 +0900

    fix: dynamic font 생성 방법 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css"
+import "./font.css"
 
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
--- src/assets/font/getSubsetFont.ts
+++ src/assets/font/getSubsetFont.ts
@@ -1,53 +1,9 @@
-import { readFile, readdir, stat } from "fs/promises"
+import { readFile } from "fs/promises"
 import { unstable_cache } from "next/cache"
-import { join } from "path"
 import subsetFont from "subset-font"
 
+import localFileCharacters from "@/assets/local-dynamic-font-targets/data.json"
 import { getPostsList, getRawPostContent } from "@/lib/octokit/blog"
-
-const readFileAndGetCharacters = async (filePath: string) => {
-  const text = await readFile(filePath, "utf-8")
-  return [...text]
-}
-
-const gatherAllCharactersRecursive = async (
-  {
-    directory,
-    filesToInclude,
-    filesToExclude,
-  }: { directory: string; filesToInclude?: RegExp; filesToExclude?: RegExp },
-  textSet = new Set<string>(),
-) => {
-  if ((await stat(directory)).isFile()) {
-    const chars = await readFileAndGetCharacters(directory)
-    chars.forEach((char) => {
-      textSet.add(char)
-    })
-    return textSet
-  }
-
-  const files = await readdir(directory)
-  for (const file of files) {
-    const filePath = join(directory, file)
-    const stats = await stat(filePath)
-    if (stats.isDirectory()) {
-      textSet = await gatherAllCharactersRecursive(
-        { directory: filePath, filesToInclude, filesToExclude },
-        new Set([...textSet]),
-      )
-    } else if (
-      (!filesToInclude || filesToInclude.test(filePath)) &&
-      (!filesToExclude || !filesToExclude.test(filePath))
-    ) {
-      const chars = await readFileAndGetCharacters(filePath)
-      chars.forEach((char) => {
-        textSet.add(char)
-      })
-    }
-  }
-
-  return textSet
-}
 
 export const getSubsetFont = ({
   fontPath,
@@ -58,28 +14,9 @@ export const getSubsetFont = ({
 }) => {
   const cached = unstable_cache(
     async () => {
-      const [postsList, textContentsFromFiles] = await Promise.all([
-        getPostsList(),
-        Promise.all([
-          gatherAllCharactersRecursive({
-            directory: join(process.cwd(), "src", "app"),
-            filesToInclude: /\.(tsx|mdx)$/,
-          }).then((characters) => [...characters]),
+      const postsList = await getPostsList()
 
-          gatherAllCharactersRecursive({
-            directory: join(process.cwd(), "src", "components"),
-            filesToInclude: /\.(tsx|mdx)$/,
-            filesToExclude: /(.*__dev__.*)/,
-          }).then((characters) => [...characters]),
-
-          gatherAllCharactersRecursive({
-            directory: join(process.cwd(), "src", "domains"),
-            filesToInclude: /\.(tsx|mdx)$/,
-          }).then((characters) => [...characters]),
-        ]).then((arr) => arr.flat()),
-      ])
-
-      const characters = new Set<string>([...JSON.stringify(postsList), ...textContentsFromFiles])
+      const characters = new Set<string>([...JSON.stringify(postsList), ...localFileCharacters])
 
       await Promise.all(
         postsList.map(async ({ slug }) => {
